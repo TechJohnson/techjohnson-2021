083e282bf85151f779381985237196fba0a5ce71
TechJohnson <steven@techjohnson.com>
2021-03-02 12:10:15 -0500

    Update footer
    An easy-to-navigate footer with social media icons and primary pages of the website.

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,36 +8,43 @@ const Footer = () => {
 query {
     twitter: file(absolutePath: { regex: "/twitter-icon.png/" }) {
         childImageSharp {
-            fixed(width: 50, quality: 100) {
+            fixed(width: 35, quality: 100) {
                 ...GatsbyImageSharpFixed
             }
         }
     }
     instagram: file(absolutePath: { regex: "/instagram-icon.png/" }) {
         childImageSharp {
-            fixed(width: 50, quality: 100) {
+            fixed(width: 35, quality: 100) {
                 ...GatsbyImageSharpFixed
             }
         }
     }
     github: file(absolutePath: { regex: "/github-icon.png/" }) {
         childImageSharp {
-            fixed(width: 50, quality: 100) {
+            fixed(width: 35, quality: 100) {
                 ...GatsbyImageSharpFixed
             }
         }
     }
     linkedin: file(absolutePath: { regex: "/linkedin-icon.png/" }) {
         childImageSharp {
-            fixed(width: 50, quality: 100) {
+            fixed(width: 35, quality: 100) {
                 ...GatsbyImageSharpFixed
             }
         }
     }
     youtube: file(absolutePath: { regex: "/youtube-icon.png/" }) {
         childImageSharp {
-            fixed(width: 50, quality: 100) {
+            fixed(width: 35, quality: 100) {
                 ...GatsbyImageSharpFixed
+            }
+        }
+    }
+    avatar: file(absolutePath: { regex: "/steven.png/" }) {
+        childImageSharp {
+            fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
             }
         }
     }
@@ -47,6 +54,26 @@ query {
     return (
         <div>
 
+            <div style={{
+                background: '#f4f4f4',
+                margin: '0',
+                paddingTop: '50px',
+                paddingBottom: '25px'
+            }}>
+                <Link to={'/'} className='siteTitle' style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    textDecoration: 'none'
+                }}>
+                    <Img fixed={data.avatar.childImageSharp.fixed} style={{ padding: '0', margin: '0' }} />
+                    <p style={{
+                        fontWeight: '600',
+                        fontSize: '1.125rem',
+                        color: '#2b2b2b',
+                        marginLeft: '10px'
+                    }}>Steven Johnson</p>
+                </Link>
+            </div>
             <div className="socialMedia" style={{
                 display: 'flex',
                 justifyContent: 'center',
@@ -69,8 +96,28 @@ query {
                 </Link> 
             </div>
 
-            <div className="container">
-                <p style={{ textAlign: 'right' }}>© {new Date().getFullYear()} Steven Johnson </p>
+            <div className='navLinks' style={{
+                display: 'flex',
+                justifyContent: 'center',
+                background: '#f4f4f4',
+                paddingTop: '25px',
+                paddingBottom: '50px'
+            }}>
+                <Link style={{ padding: '0 15px', fontWeight: '600', color: '#999999', textDecoration: 'none' }} className='menuItem' to={'/'}>About</Link>
+                <Link style={{ padding: '0 15px', fontWeight: '600', color: '#999999', textDecoration: 'none' }} className='menuItem' to={'/'}>Projects</Link>
+                <Link style={{ padding: '0 15px', fontWeight: '600', color: '#999999', textDecoration: 'none' }} className='menuItem' to={'/'}>Services</Link>
+                <Link style={{ padding: '0 15px', fontWeight: '600', color: '#999999', textDecoration: 'none' }} className='menuItem' to={'/'}>Blog</Link>
+                <Link style={{ padding: '0 15px', fontWeight: '600', color: '#999999', textDecoration: 'none' }} className='menuItem' to={'/'}>Hire Me</Link>
+            </div>
+
+            <div style={{
+                background: '#f4f4f4',
+                paddingBottom: '50px',
+                margin: '0'
+            }}>
+                <div className="container">
+                    <p style={{ padding: '0', margin: '0', textAlign: 'center', color: '#999999' }}>© {new Date().getFullYear()} Steven Johnson </p>
+                </div>
             </div>
         </div>
     )
